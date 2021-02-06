2ee045c527ab0d798081ac850437b61a611dd265
yasinyurek <yasinyurek.dev@gmail.com>
2021-02-06 16:16:09 +0300

    when the adding car : car dailyprice >0 && carModelName.Length >=2 rules added.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,25 +16,48 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        // Listeleme Fonksiyonları
         public List<Car> GetAll()
         {
-            // İş kodları
             return _carDal.GetAll();
         }
 
         public List<Car> GetAllByBrandId(int id)
         {
-            return _carDal.GetAll(p => p.BrandId == id);
+            return _carDal.GetAll(c => c.BrandId == id);
         }
 
         public List<Car> GetByColorId(int id)
         {
-            return _carDal.GetAll(p => p.ColorId == id);
+            return _carDal.GetAll(c => c.ColorId == id);
         }
 
         public List<Car> GetByDailyPrice(decimal min, decimal max)
         {
-            return _carDal.GetAll(p=> p.DailyPrice >= min && p.DailyPrice <= max);
+            return _carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max);
         }
+
+        // Emir Fonsiyonları (Ekleme Çıkarma vs)
+        public void Add(Car car)
+        {
+            if (car.DailyPrice > 0 && car.carModelName.Length >= 2)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                if (car.DailyPrice == 0)
+                {
+                    Console.WriteLine($"Lütfen günlük fiyat kısmını 0'dan büyük giriniz! Girdiğiniz fiyat : {car.DailyPrice}");
+                }
+                if (car.carModelName.Length <= 2)
+                {
+                    Console.WriteLine($"Araba ismi minimum 2 karakter olmalıdır!  Girdiğiniz isim :{car.carModelName}");
+                }
+
+            }
+        }
+
+
     }
 }
